21796f5afac3c8fa4de5e3483633344bb6d83f02
Juliette Chevalier <juliettech.1393@gmail.com>
2018-05-03 12:02:15 -0500

    how it works section added + nicer navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Lesson.destroy_all
+Teacher.destroy_all
+Booking.destroy_all
+
+20.times do
+  User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.zip_code},
+     #{Faker::Address.state}, #{Faker::Address.country}",
+    gender: ["female", "male"].sample,
+    email: Faker::Internet.email,
+    password: "password123",
+    latitude: Faker::Address.latitude,
+    longitude: Faker::Address.longitude
+    )
+end
